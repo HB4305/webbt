48921b3934a9af277f120559376dc9447d8a6f8e
HB4305 <hoaichaobai@gmail.com>
2025-10-08 12:14:42 +0700

    ud

---
+++ web.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req,res){
+    res.send('hello');
+})
+
+
+app.listen(3000, () =>
+{
+    console.log('sever is running at port ');
+})
